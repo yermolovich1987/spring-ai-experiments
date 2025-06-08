85e37d1e9da6be4d468ab352842ac8d2f6be32b0
Dmytro Iermolovych <dmytro.iermolovych@nordstrom.com>
2025-06-08 19:19:43 +0200

    Sample request to LLM

--- src/main/java/com/example/spring_ai_experiments/SampleCommandLineRunner.java
+++ src/main/java/com/example/spring_ai_experiments/SampleCommandLineRunner.java
@@ -12,15 +12,18 @@ import org.springframework.stereotype.Component;
 public class SampleCommandLineRunner implements CommandLineRunner {
 
   @Autowired
-  private OllamaChatModel ollamaChatModel;
+  private ChatClient.Builder chatClientBuilder;
 
   @Override
   public void run(String... args) {
     String prompt = "Explain how photosynthesis works in simple terms.";
     log.info("###   Prepare for calling LLM");
     try {
-      ChatClient chatClient = ChatClient.builder(ollamaChatModel).build();
-      String response = chatClient.prompt(prompt).call().content();
+      ChatClient chatClient = chatClientBuilder.build();
+      String response = chatClient.prompt()
+          .user(prompt)
+          .call()
+          .content();
       log.info("###   AI Response: {}", response);
     } catch (Exception e) {
       log.error("###   Failed to call LLM", e);
