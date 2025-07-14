6b33e3111f7becb0ce61aafeae1dd224a6812d1d
Dmytro Iermolovych <dmytro.iermolovych@nordstrom.com>
2025-07-14 13:50:35 +0200

    Sample request to LLM

--- src/main/java/com/example/spring_ai_experiments/ToolCallingCommandLineRunner.java
+++ src/main/java/com/example/spring_ai_experiments/ToolCallingCommandLineRunner.java
@@ -2,31 +2,43 @@ package com.example.spring_ai_experiments;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Slf4j
-//@Component
-public class SampleCommandLineRunner implements CommandLineRunner {
+@Component
+public class ToolCallingCommandLineRunner implements CommandLineRunner {
 
   @Autowired
   private ChatClient.Builder chatClientBuilder;
 
   @Override
   public void run(String... args) {
-    String prompt = "Explain how photosynthesis works in simple terms.";
+    String prompt = "What day is tomorrow?";
     log.info("###   Prepare for calling LLM");
     try {
       ChatClient chatClient = chatClientBuilder.build();
       String response = chatClient.prompt()
           .user(prompt)
+          .tools(new DateTimeTools())
           .call()
           .content();
       log.info("###   AI Response: {}", response);
     } catch (Exception e) {
       log.error("###   Failed to call LLM", e);
     }
+  }
+
+  private static class DateTimeTools {
+    @Tool(description = "Get the current date and time in the user's timezone")
+    String getCurrentDateTime() {
+      return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
+    }
+
   }
 }
